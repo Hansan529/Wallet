34928fb88ebf74cd43bbb2c49fe26248a269b951
hansan529 <hansan0529@gmail.com>
2023-04-06 02:02:29 +0900

    modify: 즉시 실행 함수로 변경 및 간단한 주석 작성함

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,38 @@
-// 위치에 따른 배경 색상 변경
-window.addEventListener("scroll", function () {
-  let currentScroll = document.documentElement.scrollTop; // 현재 스크롤 위치
-  let main = document.querySelector(".main");
+/** 즉시 실행하여 전역 변수,상수에 영향이 끼치지 않도록 설정함 */
+(() => {
+  const main = document.querySelector(".main");
+  /** 반응형 사이즈 체크 */
+  const mediaQueryMobile = window.matchMedia("(max-width:767px)");
+  const mediaQueryTablet = window.matchMedia(
+    "(min-width: 768px) and (max-width:1280px)"
+  );
+  const mediaQueryDesktop = window.matchMedia("(min-width:1281px)");
 
-  let maxScroll =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight; // 최대 스크롤 위치
-  console.log("maxScroll: ", maxScroll);
+  /** handleScroll 함수에서 사용 할 코드: 배경의 색상을 변경하는 함수 */
+  const setBackground = (color) => {
+    main.style.backgroundColor = color;
+  };
 
-  if (currentScroll >= 500 && currentScroll < 4100) {
-    main.style.backgroundColor = "#3496ca";
-    return;
-  } else if (currentScroll >= 4100 && currentScroll < 6500) {
-    main.style.backgroundColor = "#fbae04";
-    return;
-  } else if (currentScroll >= 6500 && currentScroll < 9400) {
-    main.style.backgroundColor = "#50be3e";
-    return;
-  } else if (currentScroll >= 9400) {
-    main.style.backgroundColor = "#f26d60";
-  } else {
-    main.style.backgroundColor = "#d9d6cc";
-  }
-});
+  /** 스크롤을 할 때, 위치 값에 따라 배경 색상을 변경하는 함수 */
+  const handleScroll = () => {
+    /** 현재 마우스의 위치를 알 수 있는 상수 */
+    const currentScroll = document.documentElement.scrollTop;
+
+    /** 반응형 사이즈 체크후, 맞다면 하단의 실행문을 이행함 */
+    if (mediaQueryMobile.matches || mediaQueryTablet.matches) {
+      if (currentScroll >= 500 && currentScroll < 4100) {
+        setBackground("#3496ca");
+      } else if (currentScroll >= 4100 && currentScroll < 6500) {
+        setBackground("#fbae04");
+      } else if (currentScroll >= 6500 && currentScroll < 9400) {
+        setBackground("#50be3e");
+      } else if (currentScroll >= 9400) {
+        setBackground("#f26d60");
+      } else {
+        setBackground("#d9d6cc");
+      }
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+})();
